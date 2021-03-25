b15b3e67527e585568f14e2ae45deee22a94cee0
Nurgle <axilles90@gmail.com>
2021-03-25 20:29:50 +0300

    Небольшие правки

--- Task№2/task№2.py
+++ Task№2/task№2.py
@@ -1,5 +1,5 @@
 """
-В нашей школе мы не модем разглашать персональные данные пользователей, но чтобы преподаватель и ученик смогли
+В нашей школе мы не можем разглашать персональные данные пользователей, но чтобы преподаватель и ученик смогли
 объяснить нашей поддержке, кого они имеют в виду ( у преподавателей, например, часто учится несколько Саш),
 мы генерируем пользователям уникальные и легко произносимые имена. Имя у нас состоит из прилогательного,
 имени животного и двузначной цифры. В итоге получается, например, "Перламутровый лось 77".
@@ -17,7 +17,7 @@ URL = [
     "https://ru.m.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83&from=%D0%90"]
 HOST = "https://ru.wikipedia.org"  # нужен изменяймый тип данных
 HEADERS = {
-    "uesr-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (HTML, like Gecko) "
                   "Version/13.0.3 Mobile/15E148 Safari/604.1", "accept": "*/*"}
 animals_dict = {}  # словарь животных
 alphabet = [chr(i) for i in range(1040, 1046)] + [chr(1025)] + [chr(j) for j in range(1046, 1072)]  # генерируем алфавит
@@ -43,7 +43,7 @@ def parse_count_page(html):
     :return: количество страниц необходимое для цикла функции get_content
     """
     soup = BeautifulSoup(html, "html.parser")
-    count_page = soup.find("div", id="mw-pages").find("p").get_text()  # показывает количесвто страниц
+    count_page = soup.find("div", id="mw-pages").find("p").get_text()  # показывает количество страниц
     count_page = re.findall("(\d+)", count_page)  # получаем список цифр, 0 индекс - количество животных на странице
     # 1-2 индекс общее количество животных
     count_animals = int(count_page[0])  # приводим строку к числу
@@ -84,8 +84,7 @@ def get_content(html):
     elif str_link_next_page.get("href") == None:
         URL[0] = link_next_page
     else:
-        link_next_page2 = HOST + str_link_next_page.get("href")  # для первой странице это пустая пследовательность
-        URL[0] = link_next_page2
+        URL[0] = HOST + str_link_next_page.get("href") # для первой страницы - это пустая последовательность
 
     return animals_dict
 
